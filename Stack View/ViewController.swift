baab613d59ce5db7cd05c6cf2b4da3b95e11b2c6
Stefani <stefani.dewi@binus.ac.id>
2019-07-03 16:15:10 +0700

    Initial Commit

---
+++ Stack View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stack View
+//
+//  Created by Stefani Kurnia Permata Dewi on 03/07/19.
+//  Copyright © 2019 Stefani Kurnia Permata Dewi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
